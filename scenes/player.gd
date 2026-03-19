d9ec979e1de3cbd1cd9eab96357920b8f22a93a3
ItzvPikachu07 <tylergoku2@gmail.com>
2026-03-20 09:27:51 +1300

    created player lazer

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,5 +1,6 @@
 extends Area2D
 
+@onready var player_lazer_scene = preload("res://scenes/player_lazer.tscn")
 @export var speed:int = 10
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -20,3 +21,10 @@ func _process(delta: float) -> void:
 
 	position.x  = clampi(position.x, 19, 1133)
 	position.y = clampi(position.y, 25, 624)
+
+
+
+	if Input.is_action_just_pressed("player_fire"):
+		var player_lazer = player_lazer_scene. instantiate()
+		player_lazer.position = position
+		get_parent().add_child(player_lazer)
